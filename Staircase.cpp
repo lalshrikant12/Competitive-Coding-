ab7d6e1528e4d5e0f6d9afd9241a133e8d6962ac
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-01-15 22:46:19 +0530

    Update Staircase.cpp

--- Staircase.cpp
+++ Staircase.cpp
@@ -9,3 +9,30 @@ Print a staircase of height N that consists of # symbols and spaces. For example
  #####
 ######
 Note: The last line has 0 spaces before it.
+
+     #include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0; i<n;++i){    
+        for (int j= 0;j<((n-i)-1);++j){
+            printf(" ");
+        }
+        for (int k=0;k<(i+1);++k){
+            printf("#");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+ Staircase - Hacker Rank Solution  
+-----------------------------------------------------------------------------------------------------------------------------
+ 
