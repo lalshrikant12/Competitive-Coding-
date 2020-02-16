2a95ecb624aac5256e72113be6996172e90fab90
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-02-16 19:01:09 +0530

    Update Min-Max.java

--- Min-Max.java
+++ Min-Max.java
@@ -1,4 +1,4 @@
-Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+/* Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
 For example, . Our minimum sum is  and our maximum sum is . We would print
 
@@ -9,7 +9,7 @@ Complete the miniMaxSum function in the editor below. It should print two space-
 
 miniMaxSum has the following parameter(s):
 
- /* arr: an array of  integers
+  arr: an array of  integers
 Input Format
 
 A single line of five space-separated integers.
@@ -26,6 +26,7 @@ Sample Input
 1 2 3 4 5
 Sample Output
 
+
 10 14
 Explanation
 
@@ -38,3 +39,54 @@ If we sum everything except , our sum is .
 If we sum everything except , our sum is .
 Hints: Beware of integer overflow! Use 64-bit Integer.
 */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Solution
+{
+public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);
+
+long m=0,M=0;
+int round,j;
+long temp;
+
+
+
+long arr[]= new long[5];
+for(int i=0;i<5;i++)
+{
+arr[i]=sc.nextLong();
+}
+
+for(round=1;round<5;round++)
+for(j=0;j<=5-1-round;j++)
+if(arr[j]>arr[j+1])
+{
+temp=arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
+}
+for(int i=0;i<5-1;i++)
+{
+
+m=m+arr[i];
+}
+for(int k=1;k<5;k++)
+{
+M=M+arr[k];
+}
+     System.out.println(m + " " + M);
+
+
+}
+
+}
+
