f02e1ca229e0f532a9590f2de30e6f925903539c
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-04-20 00:22:33 +0530

    Update Electronic shop.java

--- Electronic shop.java
+++ Electronic shop.java
@@ -1,50 +1,52 @@
-import java.util.*;
-public class Electronic {
-public static void main(String args[])
-{
-Scanner sc=new Scanner(System.in);
-int d,n,m;
-int max1=-1;
-d=sc.nextInt();
-n=sc.nextInt();
-m=sc.nextInt();
-int keyboard[]=new int[n];
-int usb[]=new int[m];
-int left[]=new int[1000];
-for(int i=0;i<n;i++)
-{
-keyboard[i]=sc.nextInt();	
-}
-for(int i=0;i<m;i++)
-{
-usb[i]=sc.nextInt();	
-}
-for(int i=0;i<n;i++)
-{
-for(int j=0;j<m;j++)
-{
-if(keyboard[i]+usb[j]<=d)
-{
-	//left[j]=keyboard[i]+usb[j];
-	left[i]=keyboard[i]+usb[j];
-	Math.max(max1,left[i]);
-	
-}
+/* Monica wants to buy a keyboard and a USB drive from her favorite electronics store. The store has several models of each. Monica wants to spend as much as possible for the  items, given her budget.
 
-}
-}
-/*int max=-1;
-for(int i=0;i<1000;i++)
-{
-if(left[i]>max)
-{
-	max=left[i];
-}
+Given the price lists for the store's keyboards and USB drives, and Monica's budget, find and print the amount of money Monica will spend. If she doesn't have enough money to both a keyboard and a USB drive, print -1 instead. She will buy only the two required items.
 
-}
+For example, suppose she has  to spend. Three types of keyboards cost . Two USB drives cost . She could purchase a , or a . She chooses the latter. She can't buy more than  items so she can't spend exactly .
+
+Function Description
+
+Complete the getMoneySpent function in the editor below. It should return the maximum total price for the two items within Monica's budget, or  if she cannot afford both items.
+
+getMoneySpent has the following parameter(s):
+
+keyboards: an array of integers representing keyboard prices
+drives: an array of integers representing drive prices
+b: the units of currency in Monica's budget
+Input Format
+
+The first line contains three space-separated integers , , and , her budget, the number of keyboard models and the number of USB drive models.
+The second line contains  space-separated integers , the prices of each keyboard model.
+The third line contains  space-separated integers , the prices of the USB drives.
+
+Constraints
+
+The price of each item is in the inclusive range .
+Output Format
+
+Print a single integer denoting the amount of money Monica will spend. If she doesn't have enough money to buy one keyboard and one USB drive, print -1 instead.
+
+Sample Input 0
+
+10 2 3
+3 1
+5 2 8
+Sample Output 0
+
+9
+Explanation 0
+
+She can buy the  keyboard and the  USB drive for a total cost of .
+
+Sample Input 1
+
+5 1 1
+4
+5
+Sample Output 1
+
+-1
+Explanation 1
+
+There is no way to buy one keyboard and one USB drive because 4+5>5, so we print -1.
 */
-System.out.println(max1);
-}
-}
-
-//this code is not currently working
