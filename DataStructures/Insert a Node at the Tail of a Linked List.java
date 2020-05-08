a0408036a859bcc304c001ad431d3c0dfd4ff7e9
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-05-09 00:51:58 +0530

    Update Insert a Node at the Tail of a Linked List.java

--- DataStructures/Insert a Node at the Tail of a Linked List.java
+++ DataStructures/Insert a Node at the Tail of a Linked List.java
@@ -38,3 +38,79 @@ First the linked list is NULL. After inserting 141, the list is 141 -> NULL.
 After inserting 302, the list is 141 -> 302 -> NULL.
 After inserting 164, the list is 141 -> 302 -> 164 -> NULL.
 After inserting 530, the list is 141 -> 302 -> 164 -> 530 -> NULL. After inserting 474, the list is 141 -> 302 -> 164 -> 530 -> 474 -> NULL, which is the final list.*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        SinglyLinkedListNode node=new SinglyLinkedListNode(data);
+        if(head==null)
+        {
+        head=node;
+        return head;
+        }
+        
+        SinglyLinkedListNode currentnode=head;
+        while(currentnode.next!=null)
+        {
+           currentnode=currentnode.next;
+
+        }
+        currentnode.next=node;
+        return head;
+
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
