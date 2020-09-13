b37215d86bd0899f9d62b3f9abea0f1cf8d5f070
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-09-13 19:52:16 +0530

    Update reversestring.cpp

--- GeeksforGeeks/reversestring.cpp
+++ GeeksforGeeks/reversestring.cpp
@@ -1,4 +1,4 @@
-Given a String of length S, reverse the whole string without reversing the individual words in it. Words are separated by dots.
+/*Given a String of length S, reverse the whole string without reversing the individual words in it. Words are separated by dots.
 
 Input:
 The first line contains T denoting the number of testcases. T testcases follow. Each case contains a string S containing characters.
@@ -19,3 +19,39 @@ pqr.mno
 Output:
 much.very.program.this.like.i
 mno.pqr
+*/
+
+#include<iostream> 
+#include<algorithm> 
+
+using namespace std;
+
+void reverseword(string& s)
+{
+
+    int start=0;
+    for(int end=0;end<s.length();end++) //reversing each word in the string
+    {
+        if(s[end]=='.')
+        {
+            reverse(s.begin()+start,s.begin()+end);
+            start=end+1;
+        }
+    }
+     reverse(s.begin() + start, s.end()); //reversiing last word
+     reverse(s.begin(),s.end()); //reverse the entire string
+}
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        string s;
+        cin>>s;
+        reverseword(s);
+        cout<<s<<endl;
+    }
+    
+    
+}
