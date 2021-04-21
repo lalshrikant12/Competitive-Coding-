b935c8a6d79a4c0fc373b726bc03fc9871903895
Shrikant Lal <46701610+lalshrikant12@users.noreply.github.com>
2021-04-22 01:47:35 +0530

    Create Odd one Out.cpp

---
+++ Hackerearth/Odd one Out.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{	ios_base::sync_with_stdio(false);// to avoid TLE
+    cin.tie(NULL);
+	long long n;
+	cin>>n;
+	vector<long int> p;
+	long long num;
+	long long sum=0;
+	for(long i=0;i<n-1;i++)
+
+     {
+
+          cin>>num;
+			p.push_back(num);
+          sum=sum+push_back[i];
+
+     }
+	 long long sumAp=n*(2+(n-1)*2)/2;;
+	 
+	
+
+	long long k=sumAp-sum;
+	cout<<k;
+	return 0;
+}
